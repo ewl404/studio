7728196ec027e5c74738f454aa283efee8054014
Wallace Basso <atendimentowallacebasso@gmail.com>
2025-06-26 09:36:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a href="#" target

--- src/components/paying-games-grid.tsx
+++ src/components/paying-games-grid.tsx
@@ -17,7 +17,7 @@ const gamesData = [
     { name: 'Fortune Tree', logo: 'https://madetoinvest.pro/wp-content/uploads/2025/06/ipad-10.jpg', dataAiHint: 'fortune tree', href: '/jogos-pagando/snake' },
     { name: 'Roleta Brasileira', logo: 'https://madetoinvest.pro/wp-content/uploads/2025/06/ipad-9.avif', dataAiHint: 'roulette wheel', href: '/jogos-pagando/zeus' },
     { name: 'Sweet Bonanza', logo: 'https://madetoinvest.pro/wp-content/uploads/2025/06/ipad-8.avif', dataAiHint: 'candy sweet', href: '/jogos-pagando/hatch' },
-    { name: 'Big Bass Bonanza', logo: 'https://madetoinvest.pro/wp-content/uploads/2025/06/ipad-7.avif', dataAiHint: 'fish bass', href: '#' },
+    { name: 'Big Bass Bonanza', logo: 'https://madetoinvest.pro/wp-content/uploads/2025/06/ipad-7.avif', dataAiHint: 'fish bass', href: '/jogos-pagando/tigerluck' },
 ];
 
 export default function PayingGamesGrid() {
