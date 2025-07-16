ec675459b91e3de04e555a73416ad49c3182fe31
Wallace Basso <atendimentowallacebasso@gmail.com>
2025-07-16 07:24:34 +0000

    volte a estrutura do video como tava antes e muda pra esse link o video:

--- src/app/lp/lp-client-page.tsx
+++ src/app/lp/lp-client-page.tsx
@@ -15,7 +15,7 @@ import { Slider } from "@/components/ui/slider";
 const SalesVideo = () => (
     <div className="w-full max-w-[420px] mx-auto relative" style={{padding: '177.78% 0 0 0'}}>
         <iframe 
-            src="https://player.vimeo.com/video/984852085?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1" 
+            src="https://player.vimeo.com/video/1101772491?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;autoplay=1" 
             frameBorder="0" 
             allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" 
             style={{position:'absolute', top:0, left:0, width:'100%', height:'100%'}} 
